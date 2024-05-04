dd56bc9cb3891d8d2b96f3d9719cb71d359e41cb
XuanSonHoang <hson512475@gmail.com>
2024-05-04 15:08:15 +0700

    feat: add token repository, response

---
+++ Booking.Core/Repositories/TokenRepository.cs
@@ -0,0 +1,50 @@
+﻿using BookingHotel.Core.IRepositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHotel.Core.Repositories
+{
+    public class TokenRepository : ITokenRepository
+    {
+        //configuration
+        private readonly IConfiguration configuration;
+        public TokenRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }   
+        public Task<string> GenerateToken(IdentityUser user, List<string> roles)
+        {
+            //create claims
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            //create token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
